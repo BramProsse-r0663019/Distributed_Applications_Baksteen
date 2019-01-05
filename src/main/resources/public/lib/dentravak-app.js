63936b71e41d21c88f81ba338d42502d934e4c42
KennethBaksteen-r0625099 <kenneth.baksteen@student.ucll.be>
2019-01-05 14:02:25 +0100

    Reload bug

--- src/main/resources/public/lib/dentravak-app.js
+++ src/main/resources/public/lib/dentravak-app.js
@@ -21,7 +21,6 @@ class DenTravakApp extends DenTravakAbstractElement{
         this.byCss(`dt-sandwich-list`).classList.remove('hidden');
         this.byCss(`dt-sandwich-checkout`).classList.add('hidden');
         this.byCss(`dt-order-confirmation`).classList.add('hidden');
-        window.location.reload(true);
     }
 
     showCheckoutPage(sandwich) {
